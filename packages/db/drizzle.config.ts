dcfd4264e84bfd910e155c7690e0c7f0991b76a9
Rutam Bhagat <rutambhagat@gmail.com>
2026-04-13 19:41:22 +0530

    feat: add drizzle migrations

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,15 +1,31 @@
 import dotenv from "dotenv";
-import { defineConfig } from "drizzle-kit";
+import { type DrizzleMigrationsConfig } from "@drepkovsky/drizzle-migrations";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 const rootEnvPath = Bun.resolveSync("../../.env", import.meta.dir);
 
 dotenv.config({ path: rootEnvPath });
 
-export default defineConfig({
+const config: DrizzleMigrationsConfig = {
   schema: "./src/schema",
   out: "./src/migrations",
   dialect: "postgresql",
+  casing: "snake_case",
   dbCredentials: {
     url: process.env.DATABASE_URL || "",
   },
-});
+  migrations: {
+    schema: "public",
+    table: "drizzle_migrations",
+  },
+  getMigrator: async () => {
+    const migrationClient = postgres(process.env.DATABASE_URL || "", {
+      max: 1,
+    });
+
+    return drizzle(migrationClient);
+  },
+};
+
+export default config;
